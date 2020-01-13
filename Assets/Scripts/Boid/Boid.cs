fcb40ed3b69bdfe0faaaa61988ecce9237ad6e4a
Drew Graham <dmg9626@gmail.com>
2020-01-12 20:01:57 -0500

    added noise to boid pathfinding

--- Assets/Scripts/Boid/Boid.cs
+++ Assets/Scripts/Boid/Boid.cs
@@ -36,6 +36,13 @@ public class Boid : MonoBehaviour
         [Range(0,1)]
         public float cohesion;
 
+        /// <summary>
+        /// Weight used to control random wandering force
+        /// </summary>
+        [SerializeField]
+        [Range(0,1)]
+        public float noise;
+
         #endregion
 
         #region DetectionSettings
@@ -110,8 +117,11 @@ public class Boid : MonoBehaviour
         Vector3 alignment = BoidDetection.GetAlignment(this, boids, settings).normalized * settings.alignment;
         Vector3 cohesion = BoidDetection.GetCohesion(this, boids, settings).normalized * settings.cohesion;
 
+        // Generate random movement vector
+        Vector3 noise = Random.insideUnitCircle * settings.noise;
+
         // Get averages of all vectors
-        Vector3 sum = (forward + separation + alignment + cohesion).normalized;
+        Vector3 sum = (forward + separation + alignment + cohesion + noise).normalized;
         Debug.DrawRay(transform.position, sum * settings.moveSpeed, Color.green);
 
         // Apply resulting rotation to boid
